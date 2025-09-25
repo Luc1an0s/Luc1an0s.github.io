9d19b5b21dbb45d9f27386a973d4835a6a597a37
Luc1an0s <lucianomaciel53@gmail.com>
2025-09-25 13:29:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active")
-  navMenu.classList.toggle("active")
-})
+if (hamburger && navMenu) {
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+  })
+}
 
 document.querySelectorAll(".nav-link").forEach((n) =>
   n.addEventListener("click", () => {
